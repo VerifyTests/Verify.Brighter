6aed022eaf324883f294eae3d3cb6eb87ce25504
Simon Cropp <simon.cropp@gmail.com>
2024-10-03 17:23:17 +1000

    refs

--- src/Verify.Brighter/Converters/RecordingCommandProcessor_Clear.cs
+++ src/Verify.Brighter/Converters/RecordingCommandProcessor_Clear.cs
@@ -5,8 +5,20 @@ public partial class RecordingCommandProcessor
     public void ClearOutbox(int amountToClear = 100, int minimumAge = 5000, Dictionary<string, object>? args = null) =>
         queue.Enqueue(new(CommandType.Clear, new ClearOutboxRecord(amountToClear, minimumAge, false, args)));
 
-    public void ClearAsyncOutbox(int amountToClear = 100, int minimumAge = 5000, bool useBulk = false, Dictionary<string, object>? args = null) =>
+    public Task ClearOutboxAsync(int amountToClear = 100, int minimumAge = 5000, Dictionary<string, object>? args = null)
+    {
         queue.Enqueue(new(CommandType.Clear, new ClearOutboxRecord(amountToClear, minimumAge, false, args)));
+        return Task.CompletedTask;
+    }
+
+    public void ClearAsyncOutbox(int amountToClear = 100, int minimumAge = 5000, bool useBulk = false, Dictionary<string, object>? args = null) =>
+        queue.Enqueue(new(CommandType.Clear, new ClearOutboxRecord(amountToClear, minimumAge, useBulk, args)));
+
+    public Task ClearAsyncOutboxAsync(int amountToClear = 100, int minimumAge = 5000, bool useBulk = false, Dictionary<string, object>? args = null)
+    {
+        queue.Enqueue(new(CommandType.Clear, new ClearOutboxRecord(amountToClear, minimumAge, useBulk, args)));
+        return Task.CompletedTask;
+    }
 
     public void ClearOutbox(params Guid[] posts) =>
         queue.Enqueue(new(CommandType.Clear, new ClearOutboxRecord(posts)));
